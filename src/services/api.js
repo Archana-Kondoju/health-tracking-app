29245bbd38bbd8c2895c56c9cf238aaadd79dbb4
Archana-Kondoju <147182547+Archana-Kondoju@users.noreply.github.com>
2024-09-17 16:34:25 +0530

    server URL added

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: 'http://trial-dnrc.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
